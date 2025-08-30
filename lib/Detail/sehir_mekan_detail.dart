da1544d5778c948e4a98e274c1492a42c060159f
Berk Altay <berkaltay3435@gmail.com>
2025-08-30 13:49:29 +0300

    konum butonu mekanların detayındaki class a taşındı

--- lib/Detail/sehir_mekan_detail.dart
+++ lib/Detail/sehir_mekan_detail.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gezinti/Model/mekan_model.dart';
+import 'package:gezinti/Model/ulke_sehir_model.dart';
 import 'package:gezinti/Widget/widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 //Her Şehirdeki Her bir mekan için ilgili kısım burada bulunur
 class SehirMekanDetail extends StatelessWidget {
   final MekanModel mekan;
   const SehirMekanDetail({super.key, required this.mekan});
+
+  Future<void> _openGoogleMaps(double lat, double lng) async {
+    final Uri googleUrl = Uri.parse(
+      'https://www.google.com/maps/search/?api=1&query=$lat,$lng',
+    );
+
+    if (await canLaunchUrl(googleUrl)) {
+      await launchUrl(googleUrl, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint('URL açılamıyor: $googleUrl');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +36,31 @@ class SehirMekanDetail extends StatelessWidget {
             topRight: Radius.circular(25),
           ),
           color: ColorWidget.blue200,
+        ),
+        child: Column(
+          children: [
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton.icon(
+                onPressed:
+                    (mekan.enlem != null && mekan.boylam != null)
+                        ? () => _openGoogleMaps(mekan.enlem!, mekan.boylam!)
+                        : null,
+                label: Text(
+                  'Konum',
+                  style: TextStyle(color: ColorWidget.white, fontSize: 20),
+                ),
+                icon: Icon(FontAwesomeIcons.locationDot),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorWidget.blue900,
+                  iconSize: 20,
+                  iconColor: ColorWidget.white,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
