55a5ca38d07ef9ed5d6ade28ab6fb58aa2fae573
Berk Altay <berkaltay3435@gmail.com>
2025-10-01 16:16:45 +0300

    turkey_detail taşınması

---
+++ lib/Detail/turkey_detail.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:gezinti/GenelDetail/sehir_detail.dart';
+import 'package:gezinti/Model/model.dart';
+import 'package:gezinti/Widget/widget.dart';
+import 'package:gezinti/Container/container.dart';
+import 'package:gezinti/Service/data_service.dart';
+
+//Türkiyedeki iller burada bulunuyor
+class TurkeyDetail extends StatelessWidget {
+  const TurkeyDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorWidget.blue900,
+      appBar: AppbarWidget(text: 'Türkiye'),
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
+          color: ColorWidget.blue200,
+        ),
+        child: FutureBuilder<List<UlkeSehirModel>>(
+          future: DataService().loadJsonData(),
+          builder: (context, snapshot) {
+            final List<UlkeSehirModel> tumSehirler = snapshot.data ?? [];
+            // 1) Önce ülke kodu ile filtrele
+            List<UlkeSehirModel> sehirler =
+                tumSehirler.where((s) => s.ulkeKodu == 'TR').toList();
+            return ListView.builder(
+              itemCount: sehirler.length,
+              itemBuilder: (context, index) {
+                final sehir = sehirler[index];
+                return TurkeyDetailContainer(
+                  ozellikSehir: sehir,
+                  onPressedButton: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SehirDetail(sehir: sehir),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
